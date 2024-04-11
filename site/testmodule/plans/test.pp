d6e0fe1ffd2502099d2664f5a9e0db34374d3d24
Tim Meusel <tim@bastelfreak.de>
2024-04-11 14:56:24 +0200

    updates

--- site/testmodule/plans/test.pp
+++ site/testmodule/plans/test.pp
@@ -1,4 +1,18 @@
 plan testmodule::test {
-  $nodes = puppetdb_query('resources[certname] { type = "Class" and title in [ "Puppet_enterprise::Profile::Master", "Puppet_enterprise::Profile::Puppetdb"] group by certname }').map |$fqdn| {$fqdn['certname']}
-  out::message($nodes)
+  $primary = puppetdb_query('resources[certname] { type = "Class" and title in [ "Puppet_enterprise::Profile::Certificate_authority", "Puppet_enterprise::Profile::Database"] group by certname }').map |$fqdn| {$fqdn['certname']}
+  $legacy_primary = puppetdb_query('resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Certificate_authority" group by certname }').map |$fqdn| {$fqdn['certname']} - $primary
+  $replica = puppetdb_query('resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Primary_master_replica" group by certname }').map |$fqdn| {$fqdn['certname']}
+  $compiler = puppetdb_query('resources[certname] { type = "Class" and title in [ "Puppet_enterprise::Profile::Master", "Puppet_enterprise::Profile::Puppetdb"] group by certname }').map |$fqdn| {$fqdn['certname']} - $primary
+  $legacy_compiler = puppetdb_query('resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Master" group by certname }').map |$fqdn| {$fqdn['certname']} - $compiler
+  $postgres = puppetdb_query('resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Database" group by certname }').map |$fqdn| {$fqdn['certname']} - $primary
+
+  $data = {
+    'primary'         => $primary,
+    'legacy_primary'  => $legacy_primary,
+    'replica'         => $replica,
+    'compiler'        => $compiler,
+    'legacy_compiler' => $legacy_compiler,
+    'postgres'        => $postgres,
+  }
+  return $data
 }

6c70dc6e2efbb605f55896dacb47dc55e1460184
Tim Meusel <tim@bastelfreak.de>
2024-05-03 13:39:40 +0200

    foo

--- site/profiles/manifests/test.pp
+++ site/profiles/manifests/test.pp
@@ -7,7 +7,7 @@ class profiles::test {
   # create a new bolt project
   bolt::project { 'peadmmig': }
 
-  -> file { '/opt/peadmmig/convert.json':
+  -> file { '/opt/peadmmig/test::convert.json':
     owner   => 'peadmmig',
     group   => 'peadmmig',
     content => { 'primary_host' => $facts['networking']['fqdn'] }.stdlib::to_json_pretty,

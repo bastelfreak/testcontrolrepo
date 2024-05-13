6dc6b8cdd7a8f0ad7719db1e5d537af34e4b498f
Tim Meusel <tim@bastelfreak.de>
2024-05-13 16:05:15 +0200

    add fact

--- site/profiles/manifests/cleanup.pp
+++ site/profiles/manifests/cleanup.pp
@@ -17,29 +17,38 @@ class profiles::cleanup {
       fail('\'puppet_enterprise::master::code_manager::sources\' needs 1 or more repos with prefix=>true')
     }
     if ($sources.length - $with_prefix.length) != 1 {
-      fail("'puppet_enterprise::master::code_manager::sources\' can only have one repo with prefix=>false. But it's set to ${sources}")
+      fail("'puppet_enterprise::master::code_manager::sources' can only have one repo with prefix=>false. But it's set to ${sources}")
     }
   }
-  $group = 'PE Master'
-  $data = dig(node_groups($group))[$group]
-  $classes = dig($data, 'classes')
-  if $classes {
-    if dig($classes, 'puppet_enterprise::profile::master', 'r10k_remote') {
-      $master = { 'puppet_enterprise::profile::master' => $classes['puppet_enterprise::profile::master'] - 'r10k_remote' }
-      $new_classes = $classes + $master
-      node_group { $group:
-        classes        => $new_classes,
-        purge_behavior => 'classes',
-        # purge read only attributes + classes
-        *              => $data - ['environment_trumps', 'last_edited', 'serial_number', 'config_data', 'id','classes',],
+
+  # cleanup the node classifier data only when the hiera settings are already written
+  # this ensures that we don't brick our deployment (assume the initial run removes the data from the node classifier and wants to update code-manager config via hiera but that fails.
+  if fact('codemanager_config.sources') {
+    $group = 'PE Master'
+    $data = dig(node_groups($group))[$group]
+    $classes = dig($data, 'classes')
+    if $classes {
+      if dig($classes, 'puppet_enterprise::profile::master', 'r10k_remote') {
+        $master = { 'puppet_enterprise::profile::master' => $classes['puppet_enterprise::profile::master'] - 'r10k_remote' }
+        $new_classes = $classes + $master
+        node_group { $group:
+          classes        => $new_classes,
+          purge_behavior => 'classes',
+          # purge read only attributes + classes
+          *              => $data - ['environment_trumps', 'last_edited', 'serial_number', 'config_data', 'id','classes',],
+        }
+      } else {
+        echo { "r10k_remote not set in node group ${group}":
+          withpath => false,
+        }
       }
     } else {
-      echo { "r10k_remote not set in node group ${group}":
+      echo { "no classes hash in node group ${group}":
         withpath => false,
       }
     }
   } else {
-    echo { "no classes hash in node group ${group}":
+    echo { '\'puppet_enterprise::master::code_manager::sources\' in hiera but not yet written to the config. Not updating PE classifier node groups':
       withpath => false,
     }
   }

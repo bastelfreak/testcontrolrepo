e419914b70e980ed483ca18c19844d988993af36
Tim Meusel <tim@bastelfreak.de>
2024-06-21 14:21:16 +0200

    add site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
-$path = '/tmp/numbers'
-file { $path:
+$fpath = '/tmp/numbers'
+file { $fpath:
   ensure => 'file',
 }
 range(0, 10000).each |$element| {
   file_line { $element:
-    path => $path,
+    path => $fpath,
     line => "${element}\n",
   }
 }

acf2886561889041eb47f98a64f16f1f5de7d99f
Tim Meusel <tim@bastelfreak.de>
2024-06-21 15:41:56 +0200

    updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,14 +11,8 @@ file { '/tmp/numbers':
 
 $fpath = '/tmp/numbers'
 range(0, 4000).each |$element| {
-  $require = if ($element % 2) == 0 {
-    [File[$fpath],Package['openssh']]
-  } else {
-    [File[$fpath],Package['htop']]
-  }
   file_line { "${element}-${fpath}":
     path    => $fpath,
     line    => "${element}\n",
-    require => $require,
   }
 }

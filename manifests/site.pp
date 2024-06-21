ea135b01cfcec467cd9c10d7ce31b333ecb68ffb
Tim Meusel <tim@bastelfreak.de>
2024-06-21 14:22:38 +0200

    add site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,7 @@ file { $fpath:
   ensure => 'file',
 }
 range(0, 10000).each |$element| {
-  file_line { $element:
+  file_line { "${element}-${fpath}":
     path => $fpath,
     line => "${element}\n",
   }

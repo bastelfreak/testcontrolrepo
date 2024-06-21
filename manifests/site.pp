c802a64d8fcecafcc81f9ce55a69715cff792c04
Tim Meusel <tim@bastelfreak.de>
2024-06-21 14:49:40 +0200

    updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,23 @@
+stage { 'pre': }
+stage { 'main': }
+stage { 'post': }
+stage { 'postpost': }
+
+Stage['pre'] -> Stage['main'] -> Stage['post'] -> Stage['postpost']
+
 $fpath = '/tmp/numbers'
+
 file { $fpath:
   ensure => 'file',
+  stage  => 'pre',
 }
 package { 'htop':
   ensure => 'installed',
+  stage  => 'pre',
 }
 package { 'openssh':
   ensure => 'installed',
+  stage  => 'main',
 }
 range(0, 9000).each |$element| {
   $require = if ($element % 2) == 0 {
@@ -18,5 +29,11 @@ range(0, 9000).each |$element| {
     path    => $fpath,
     line    => "${element}\n",
     require => $require,
+    stage   => 'post',
   }
+}
+
+package { 'pe-r10k':
+  ensure => 'installed',
+  stage  => 'postpost',
 }

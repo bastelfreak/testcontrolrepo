0b1e1908765683e2cd2bc680305dac912e872adb
Tim Meusel <tim@bastelfreak.de>
2024-07-19 12:19:23 +0200

    updates

---
+++ site/profiles/lib/facter/codemanager_config.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# @author Tim 'bastelfreak' Meusel <tim@bastelfreak.de>
+
+require 'yaml'
+
+Facter.add(:codemanager_config) do
+  path = '/opt/puppetlabs/server/data/code-manager/r10k.yaml'
+  confine { File.exist? path }
+  setcode do
+    YAML.load_file(path)
+  end
+end

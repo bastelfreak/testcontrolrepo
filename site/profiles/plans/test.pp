cd1301d3dade3eedae92fcfad444e01e7b23cbbe
Tim Meusel <tim@bastelfreak.de>
2024-07-30 11:26:33 +0200

    add test plan

---
+++ site/profiles/plans/test.pp
@@ -0,0 +1,10 @@
+plan profiles::test {
+  $primary_host = 'pe.tim.betadots.training'
+
+  $main  = {'action'> 'get', 'value' => 'environment', 'section' => 'main'}
+  $agent = {'action'> 'get', 'value' => 'environment', 'section' => 'agent'}
+
+  $foo = run_task('puppet_conf', $main, 'targets' => $primary_host, '_run_as' => 'root')
+
+  out::message($foo)
+}

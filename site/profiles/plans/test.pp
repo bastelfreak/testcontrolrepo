35d961d5ab4bb0399dadb2886f6bc9e162ed6bdc
Tim Meusel <tim@bastelfreak.de>
2024-07-30 11:47:26 +0200

    typo

--- site/profiles/plans/test.pp
+++ site/profiles/plans/test.pp
@@ -4,7 +4,7 @@ plan profiles::test {
   $main  = {'action'                              => 'get', 'value' => 'environment', 'section' => 'main', '_run_as' => 'root'}
   $agent = {'action'                              => 'get', 'value' => 'environment', 'section' => 'agent', '_run_as' => 'root'}
 
-  $foo = run_task('puppet_conf', 'targets' => $primary_host, 'args' => $main)
+  $foo = run_task('puppet_conf', $primary_host, 'description', $main)
 
   out::message($foo)
 }

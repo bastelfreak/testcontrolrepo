eb89fa9774e4c0f44de7bf35a7f3bf3408c79afc
Tim Meusel <tim@bastelfreak.de>
2024-07-30 14:34:43 +0200

    typo

--- site/profiles/plans/test.pp
+++ site/profiles/plans/test.pp
@@ -12,7 +12,7 @@ plan profiles::test {
   $agent_results = run_task('puppet_conf', $primary_host, 'description', $agent)
   $agent_env = $agent_results.results[0].value['status']
 
-  $puppetdb = puppetdb_query("nodes[catalog_environment]{ certname = \"${primary_host}\"}")
-  $puppetdb_env = puppetdb_query[0]['catalog_environment']
+  $puppetdb_results = puppetdb_query("nodes[catalog_environment]{ certname = \"${primary_host}\"}")
+  $puppetdb_env = puppetdb_results[0]['catalog_environment']
   out::message("configured environments: main section: ${main_env}; agent section: ${agent_env}; last used env: ${puppetdb_env}")
 }

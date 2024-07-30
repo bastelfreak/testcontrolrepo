05cf27c8e296fd0a5eda752cbbe9fb5f9bfd4a12
Tim Meusel <tim@bastelfreak.de>
2024-07-30 11:45:30 +0200

    typo

--- site/profiles/plans/test.pp
+++ site/profiles/plans/test.pp
@@ -1,10 +1,10 @@
 plan profiles::test {
   $primary_host = 'pe.tim.betadots.training'
 
-  $main  = {'action' => 'get', 'value' => 'environment', 'section' => 'main'}
-  $agent = {'action' => 'get', 'value' => 'environment', 'section' => 'agent'}
+  $main  = {'action'                              => 'get', 'value' => 'environment', 'section' => 'main', '_run_as' => 'root'}
+  $agent = {'action'                              => 'get', 'value' => 'environment', 'section' => 'agent', '_run_as' => 'root'}
 
-  $foo = run_task('puppet_conf', $main, 'targets' => $primary_host, '_run_as' => 'root')
+  $foo = run_task('puppet_conf', 'targets' => $primary_host, 'args' => $main)
 
   out::message($foo)
 }

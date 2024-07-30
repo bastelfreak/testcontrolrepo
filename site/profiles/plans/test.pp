f5b3ca6db49d7238d895834bb70e35a240a80c37
Tim Meusel <tim@bastelfreak.de>
2024-07-30 14:32:54 +0200

    typo

--- site/profiles/plans/test.pp
+++ site/profiles/plans/test.pp
@@ -4,11 +4,15 @@ plan profiles::test {
   $main  = {'action' => 'get', 'setting' => 'environment', 'section' => 'main', '_run_as' => 'root'}
   $agent = {'action' => 'get', 'setting' => 'environment', 'section' => 'agent', '_run_as' => 'root'}
 
-  $foo = run_task('puppet_conf', $primary_host, 'description', $main)
+  $main_results = run_task('puppet_conf', $primary_host, 'description', $main)
+  $main_env = $agent_results.results[0].value['status']
 
-  out::message($foo.results[0].value['status'])
+  #out::message($foo.results[0].value['status'])
 
-  $foo2 = run_task('puppet_conf', $primary_host, 'description', $agent)
+  $agent_results = run_task('puppet_conf', $primary_host, 'description', $agent)
+  $agent_env = $agent_results.results[0].value['status']
 
-  out::message($foo2.results[0].value)
+  $puppetdb = puppetdb_query("nodes[catalog_environment]{ certname = \"${primary_host}\"}")
+  $puppetdb_env = puppetdb_query[0]['catalog_environment']
+  out::message("configured environments: main section: ${main_env}; agent section: ${agent_env}; last used env: ${puppetdb_env}")
 }

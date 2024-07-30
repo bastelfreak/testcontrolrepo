6230b8b725afc16da4df75777e6f07b990cb8720
Tim Meusel <tim@bastelfreak.de>
2024-07-30 14:56:00 +0200

    typo

--- site/profiles/plans/test.pp
+++ site/profiles/plans/test.pp
@@ -15,4 +15,17 @@ plan profiles::test {
   $puppetdb_results = puppetdb_query("nodes[catalog_environment]{ certname = \"${primary_host}\"}")
   $puppetdb_env = $puppetdb_results[0]['catalog_environment']
   out::message("configured environments: main section: ${main_env}; agent section: ${agent_env}; last used env: ${puppetdb_env}")
+
+  # the environment used in puppet.conf or the default env
+  $conf_env = if $agent_env != $main_env {
+    out::message("'environment' set in agent section in puppet.conf: ${agent_env}")
+    $agent_env
+  } else {
+    out::message("Using 'environment' from main (default is production if not set at all): ${main_env}")
+    $main_env
+  }
+
+  if $conf_env != $puppetdb_env {
+    fail("The ENC provided environment '${puppetdb_env}' is not set in puppet.conf (${conf_env})")
+  }
 }

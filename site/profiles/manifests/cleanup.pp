4b1b660007ad2c3c82cee5a75a919b4e3733d096
Tim Meusel <tim@bastelfreak.de>
2024-07-31 09:43:24 +0200

    try and error

--- site/profiles/manifests/cleanup.pp
+++ site/profiles/manifests/cleanup.pp
@@ -116,7 +116,8 @@ class profiles::cleanup (
     $pe = profiles::readhocon($pepath)
 
     # { 'config_is_correct' => true, 'correct_env' => true, }
-    $validated_env = profiles::environment($trusted['certname'])
+    #$validated_env = profiles::environment($trusted['certname'])
+    $validated_env = { 'config_is_correct' => false, 'correct_env' => true, }
     if $validated_env['config_is_correct'] == false or $pe['puppet_enterprise::profile::master::r10k_remote'] {
       $pe_wo_remote = if $pe['puppet_enterprise::profile::master::r10k_remote'] {
         echo {'pe.conf contains puppet_enterprise::profile::master::r10k_remote, removing it':

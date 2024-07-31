200eb2fdd9a4da6e13a49c7adecd80ba846ad7d5
Tim Meusel <tim@bastelfreak.de>
2024-07-31 10:21:16 +0200

    updates

---
+++ upgrade_start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+primary="$(puppet config print server)"
+auth_header="X-Authentication: $(puppet-access show)"
+type_header='Content-Type: application/json'
+uri="https://$primary:8143/orchestrator/v1/command/task"
+taskdata='
+{ "scope": {
+    "nodes": [ "'${primary}'" ] },
+  "params": {
+    "action": "start",
+    "name": "peadmmig@profiles::upgradeto2021.service" },
+  "task": "service::linux",
+  "environment": "production"
+}'
+
+echo '# PAYLOAD'
+echo "$taskdata" | jq .
+
+curl --silent --insecure --header "$type_header" --header "$auth_header" --request POST "$uri" --data "$taskdata" | jq .

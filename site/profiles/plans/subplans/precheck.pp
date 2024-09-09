8fe7d0a5b449b43f8101448c0caba4c9bd1beed1
Tim Meusel <tim@bastelfreak.de>
2024-09-09 15:31:48 +0200

    fix precheck

--- site/profiles/plans/subplans/precheck.pp
+++ site/profiles/plans/subplans/precheck.pp
@@ -4,6 +4,10 @@
 # @param primary_host the FQDN/common name of the primary
 # @param validate_environment if we should ensure that the environment is configured properly everywhere
 # @param validate_health check if all agents are working and the environment is healthy
+# @param runs_via_bolt configure if this plan and all subplans/tasks are executed via bolt or PE. Some PE functions have different signatures
+#
+# @see https://www.puppet.com/docs/pe/latest/plans_limitations.html
+# @see https://github.com/puppetlabs/puppet-enterprise_issues/issues/5
 #
 # @api private
 #
@@ -13,20 +17,24 @@ plan profiles::subplans::precheck (
   Peadm::SingleTargetSpec $primary_host,
   Boolean $validate_environment = true,
   Boolean $validate_health = true,
+  Boolean $runs_via_bolt = true,
 ) {
   # To have a clean report, we trigger a puppet run here
   # we  run it twice, in case we've a raise condition with an already running puppet agent
-  $run_as == system::env('RUNS_VIA_BOLT') =~ String[1] {
-    {'_run_as' => 'root'}
+  $result = if $runs_via_bolt == true {
+    run_task('peadm::puppet_runonce', $primary_host, '_run_as' => 'root', '_catch_errors' => true })
   } else {
-    {}
+    run_task('peadm::puppet_runonce', $primary_host, '_catch_errors' => true )
   }
-  $result = run_task('peadm::puppet_runonce', $primary_host, $run_as + { '_catch_errors' => true })
   # ok is true if the task was successful on all targets
   unless $result.ok {
     out::message("Final peadm::puppet_runonce failed with: ${result}")
     out::message('Trying another puppet run')
-    run_task('peadm::puppet_runonce', $primary_host, $run_as)
+    if $runs_via_bolt == true {
+      run_task('peadm::puppet_runonce', $primary_host, $run_as)
+    } else {
+      run_task('peadm::puppet_runonce', $primary_host)
+    }
   }
 
   # check if the used environment is configured everywhere so agents don't flap by accident

f4606935daabf8d142dc711bad4f6012c4e6894d
Tim Meusel <tim@bastelfreak.de>
2024-11-07 10:33:51 +0100

    upgrade: download archive in a complicated way

--- site/profiles/plans/upgrade.pp
+++ site/profiles/plans/upgrade.pp
@@ -4,6 +4,7 @@
 # @param primary_host the FQDN/common name of the primary
 # @param version always points to the latest LTS
 # @param pe_installer_source optional URL to the PE builds, can point to a webdir or absolute URL
+# @param download_mode if peadm should download the installer and upload to targets, or if targets should download it on their own
 #
 # @author Tim Meusel <tim@bastelfreak.de>
 #
@@ -11,10 +12,47 @@ plan profiles::upgrade (
   Peadm::SingleTargetSpec $primary_host,
   Peadm::Pe_version $version = '2021.7.8',
   Optional[Stdlib::HTTPSUrl] $pe_installer_source = undef,
+  Enum['direct','bolthost']  $download_mode = 'bolthost',
 ) {
+
+  # In their infinite wisdom, the Puppet security team (a single person that doesn't know the product),
+  # decided that it's insecure when PEADM can download the installer archive from an internal mirror
+  # Because of that, we need to download the installer before doing the upgrade.
+  # The peadm::upgrade plan will check if the archive is available locally and then won't try to download it from AWS
+  # Of course AWS is way more trustworthy than a local mirror
+  # * https://github.com/puppetlabs/puppetlabs-peadm/pull/465/
+  # * https://github.com/puppetlabs/puppetlabs-peadm/pull/524/
+  if $pe_installer_source {
+    # custom URL ends with /, so we assume it's a webdir with the original installer
+    $pe_tarball_source = if $pe_installer_source[-1] == '/' {
+      $platform          = run_task('peadm::precheck', $primary_host).first['platform']
+      $pe_tarball_name   = "puppet-enterprise-${version}-${platform}.tar.gz"
+      out::message("pe_installer_source is a relative URL, downloading: ${pe_installer_source}${pe_tarball_name}")
+      "${pe_installer_source}${pe_tarball_name}"
+    } else {
+      out::message("pe_installer_source is an absolute URL: ${pe_installer_source}")
+      $pe_installer_source
+    }
+    # Now download the archive
+    if $download_mode == 'bolthost' {
+      $upload_tarball_path = "/tmp/${pe_tarball_name}"
+      run_plan('peadm::util::retrieve_and_upload', $primary_host,
+        source      => $pe_tarball_source,
+        local_path  => "/tmp/${pe_tarball_name}",
+        upload_path => $upload_tarball_path,
+      )
+    } else {
+      run_task('peadm::download', $primary_host,
+        source => $pe_tarball_source,
+        path   => $upload_tarball_path,
+      )
+    }
+  } else {
+    out::message('pe_installer_source not set, PE installer archive will be downloaded from the PEADM default')
+  }
   run_plan('profiles::subplans::precheck', { 'primary_host' => $primary_host })
 
-  run_plan('peadm::upgrade', { 'primary_host' => $primary_host, 'version' => $version, 'pe_installer_source' => $pe_installer_source, '_run_as' => 'root' }.delete_undef_values)
+  run_plan('peadm::upgrade', { 'primary_host' => $primary_host, 'version' => $version, '_run_as' => 'root' }.delete_undef_values)
 
   # peadm::upgrade doesn't do a final puppet run without changed resources
   # To have a clean report, we trigger a puppet run here

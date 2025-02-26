07544caaaea62e6e6ef8f1264a3d7a34d48852ba
Tim Meusel <tim@bastelfreak.de>
2025-02-26 09:27:32 +0100

    add +x

--- convert_status.sh
+++ convert_status.sh
@@ -12,23 +12,24 @@ taskdata='
     "nodes": [ "'${primary}'" ] },
   "params": {
     "action": "status",
-    "name": "peadmmig@profiles::convert2.service" },
+    "name": "peadmmig@profiles::convert.service" },
   "task": "service::linux",
   "environment": "peadm"
 }'
 
 echo '# URL'
-echo $curl
+echo "$curl"
 echo '# PAYLOAD'
 echo "$taskdata" | jq .
 
 #curl --insecure --header "$type_header" --header "$auth_header" --request POST "$uri" --data "$taskdata"
-url=$(${curl} --header "$type_header" --header "$auth_header" --request POST "$uri" --data "$taskdata" | jq --raw-output .job.id)
+#url=$(${curl} --header "$type_header" --header "$auth_header" --request POST "$uri" --data "$taskdata" | jq --raw-output .job.id)
+url=$($curl --header "$type_header" --header "$auth_header" --request POST "$uri" --data "$taskdata" | jq --raw-output .job.id)
 
 # folgendes pollen bis .state == finished
 echo '# RESPONSE 1'
 
-${curl} --request GET "${url}" | jq --raw-output .state
+$curl --header "$type_header" --header "$auth_header" --request GET "${url}" | jq --raw-output .state
 
 echo '# RESPONSE 2'
 
@@ -38,3 +39,46 @@ ${curl} --request GET "${url}" | jq --raw-output .state
 echo '# RESPONSE 3'
 #${curl} --header "$type_header" --header "$auth_header" "${url}/events" | jq '.items | .[] | select(.type=="node_finished").details.detail.status' --raw-output
 ${curl} --header "$type_header" --header "$auth_header" "${url}/events" | jq .
+
+# response is something like this:
+# {
+#  "next-events": {
+#    "id": "https://pe.local:8143/orchestrator/v1/jobs/6/events?start=19",
+#    "event": "19"
+#  },
+#  "items": [
+#    {
+#      "type": "node_running",
+#      "timestamp": "2025-02-26T08:23:04Z",
+#      "id": "16",
+#      "details": {
+#        "node": "pe.local",
+#        "detail": {},
+#        "transport": "pcp"
+#      }
+#    },
+#    {
+#      "type": "node_finished",
+#      "timestamp": "2025-02-26T08:23:04Z",
+#      "id": "17",
+#      "details": {
+#        "node": "pe.tim.betadots.training",
+#        "detail": {
+#          "status": "MainPID=0,LoadState=loaded,ActiveState=inactive",
+#          "enabled": "static"
+#        },
+#        "transport": "pcp"
+#      }
+#    },
+#    {
+#      "type": "job_finished",
+#      "timestamp": "2025-02-26T08:23:04Z",
+#      "id": "18",
+#      "details": {
+#        "state": "finished"
+#      }
+#    }
+#  ]
+#}
+
+# important is items -> type->node_finished->details->detail->status. has to be "MainPID=0,LoadState=loaded,ActiveState=inactive"

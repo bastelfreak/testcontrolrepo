ed6eaf21ce69322e45ab62088a439e9904b07eac
Tim Meusel <tim@bastelfreak.de>
2025-02-27 13:52:37 +0100

    clarify classifier

--- site/profiles/manifests/cleanup/classifier.pp
+++ site/profiles/manifests/cleanup/classifier.pp
@@ -6,9 +6,10 @@
 # @author Tim Meusel <tim@bastelfreak.de>
 #
 class profiles::cleanup::classifier {
-  # ensure agents are configured to update themself
   # when the option isn't set, the classifier won't work as a Hiera backend
   # the backend is required for PEADM
+  # the Hiera backend classifier_data, usually defined in `/etc/puppetlabs/puppet/hiera.yaml`,
+  # hardcodes `puppet_enterprise_classifier_data_backend_present` to true
   $classifier = lookup('puppet_enterprise_classifier_data_backend_present', Boolean, 'first', 'false')
   unless $classifier {
     fail('puppet_enterprise_classifier_data_backend_present needs to be set to `true` in Hiera!')

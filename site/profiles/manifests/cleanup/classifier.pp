7a0a0be2c9bba1102e418cde083150904fd4d82b
Tim Meusel <tim@bastelfreak.de>
2025-03-11 13:38:57 +0100

    fix typo

--- site/profiles/manifests/cleanup/classifier.pp
+++ site/profiles/manifests/cleanup/classifier.pp
@@ -10,7 +10,7 @@ class profiles::cleanup::classifier {
   # the backend is required for PEADM
   # the Hiera backend classifier_data, usually defined in `/etc/puppetlabs/puppet/hiera.yaml`,
   # hardcodes `puppet_enterprise_classifier_data_backend_present` to true
-  $classifier = lookup('puppet_enterprise_classifier_data_backend_present', Boolean, 'first', 'false')
+  $classifier = lookup('puppet_enterprise_classifier_data_backend_present', Boolean, 'first', false)
   unless $classifier {
     fail('puppet_enterprise_classifier_data_backend_present needs to be set to `true` in Hiera!')
   }

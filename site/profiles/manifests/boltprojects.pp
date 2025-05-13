83c7b0d22f89742e662f6e1cf7d503ba52e641ae
Tim Meusel <tim@bastelfreak.de>
2025-05-13 21:11:25 +0200

    add support for 2025.3.0

--- site/profiles/manifests/boltprojects.pp
+++ site/profiles/manifests/boltprojects.pp
@@ -12,7 +12,7 @@ class profiles::boltprojects (
   Peadm::Pe_version $version = '2021.7.10',
   Peadm::Pe_version $version_2021 = $version,
   Peadm::Pe_version $version_2023 = '2023.8.2',
-  Peadm::Pe_version $version_2025 = '2025.1.0',
+  Peadm::Pe_version $version_2025 = '2025.3.0',
 ) {
   # create a new bolt project
   bolt::project { 'peadmmig': }

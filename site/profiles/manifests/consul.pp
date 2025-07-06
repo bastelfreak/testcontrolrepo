372b1410e76bec8149c8fbbe2681e5536ec54599
Tim Meusel <tim@bastelfreak.de>
2025-07-06 15:29:52 +0200

    bind all

--- site/profiles/manifests/consul.pp
+++ site/profiles/manifests/consul.pp
@@ -1,4 +1,9 @@
 class profiles::consul {
+  $nodes = [
+    'server1.tim.betadots.training',
+    'server2.tim.betadots.training',
+    'server3.tim.betadots.training',
+  ]
   class { 'consul':
     install_method  => 'package',
     manage_repo     => $facts['os']['name'] != 'Archlinux',
@@ -10,11 +15,53 @@ class profiles::consul {
     config_hash     => {
       'server'    => true,
       'bind_addr' => '[::]',
+        'retry_join'                 => sort($nodes),
+        'tls'                        => {
+          'defaults'    => {
+            'verify_outgoing' => true,
+            'verify_incoming' => true,
+            'ca_file'         => '/etc/consul.d/ca.pem',
+            'cert_file'       => "/etc/consul.d/${trusted['certname']}_cert.pem",
+            'key_file'        => "/etc/consul.d/${trusted['certname']}_key.pem",
+          },
+        },
+        'server_name'                => $trusted['certname'],
+        'node_name'                  => $trusted['certname'],
+        'disable_update_check'       => true,
+        'enable_local_script_checks' => true,
     },
   }
   systemd::dropin_file { 'foo.conf':
     unit           => 'consul.service',
     content        => "[Unit]\nConditionFileNotEmpty=\nConditionFileNotEmpty=/etc/consul.d/config.json",
     notify_service => true,
+  }
+  $cert = "/etc/puppetlabs/puppet/ssl/private_keys/${trusted['certname']}.pem"
+  file { "/etc/consul.d/${trusted['certname']}_key.pem":
+    ensure  => 'file',
+    owner   => 'consul',
+    group   => 'consul',
+    mode    => '0400',
+    source  => $cert,
+    require => Package['consul'],
+    notify  => Service['consul'],
+  }
+  file { "/etc/consul.d/${trusted['certname']}_cert.pem":
+    ensure  => 'file',
+    owner   => 'consul',
+    group   => 'consul',
+    mode    => '0400',
+    source  => "/etc/puppetlabs/puppet/ssl/certs/${trusted['certname']}.pem",
+    require => Package['consul'],
+    notify  => Service['consul'],
+  }
+  file { '/etc/consul.d/ca.pem':
+    ensure  => 'file',
+    owner   => 'consul',
+    group   => 'consul',
+    mode    => '0400',
+    source  => '/etc/puppetlabs/puppet/ssl/certs/ca.pem',
+    require => Package['consul'],
+    notify  => Service['consul'],
   }
 }

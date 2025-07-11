47f812d98352fca172cb691d0833abab36358567
Tim Meusel <tim@bastelfreak.de>
2025-07-11 13:47:30 +0200

    simulate erorr

--- site/profile/tasks/recertify_node_yolo.rb
+++ site/profile/tasks/recertify_node_yolo.rb
@@ -5,6 +5,7 @@ require 'json'
 data = JSON.parse(STDIN.read)
 
 if data['myfunction'] = 'download_p2a_csr' && data['singlenode'] == 'pe.tim.betadots.training'
+  puts 'ahhhh karpott'
   exit 2
 else
   puts data
